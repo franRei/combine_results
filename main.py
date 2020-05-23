11aecc54a9c42c922e1b0c6a67c3462cc1a19563
franRei <38581197+franRei@users.noreply.github.com>
2020-05-23 14:24:09 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,18 +2,15 @@ from Reader import Reader
 from Combine import Combine
 import csv
 
-filename_kroonen = "files/cognate-sets-kroonen2013.csv"
-filename_dellert_cognates = "files/cognates.csv"
-filename_dellert_forms = "files/forms.csv"
+filename_kroonen = "files/cognate-sets-kroonen2013.csv" 
+filename_cognates = "files/cognates.csv"
+filename_forms = "files/forms.csv"
 
 if __name__ == '__main__':
     Reader = Reader()
     kroonen = Reader.read_csv(filename_kroonen)
-    cognates, com_cognates = Reader.read_csv_cognates(filename_dellert_cognates)
-    forms, com_forms = Reader.read_csv_forms(filename_dellert_forms)
-
-
-    print(cognates.get("19564"))
+    cognates, com_cognates = Reader.read_csv_cognates(filename_cognates)
+    forms, com_forms = Reader.read_csv_forms(filename_forms)
 
     Combine = Combine()
     combined = Combine.combine_lists(kroonen, cognates, forms)
